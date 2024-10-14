22894936ff040f5816086f6f13c046a565ee33d7
Farhan Fansuri <farhanfansuri2023@gmail.com>
2024-10-14 20:43:58 +0700

    update(files): menambahkan beberapa file

--- docker-container.sh
+++ docker-container.sh
@@ -1,11 +1,3 @@
-docker version
-
-sudo docker image ls
-
-sudo docker pull nama_image
-
-sudo docker image rm nama_image
-
 sudo docker container ls -a
 
 sudo docker container create --name nama_container image:tag
@@ -23,3 +15,5 @@ sudo docker container exec -i -t  container_id/name program
 sudo docker container create --name cntainer_name --publish porthost:portcontainer image:tag
 
 sudo docker container create --name cntainer_name --env KEY="value" image:tag
+
+sudo docker container stats
---
+++ docker-image.sh
@@ -0,0 +1,5 @@
+sudo docker image ls
+
+sudo docker pull nama_image
+
+sudo docker image rm nama_image
