c50fd07e1031ae16d9afab6bc4cda99e80e37ed1
Farhan Fansuri <farhanfansuri2023@gmail.com>
2024-10-14 17:55:01 +0700

    create(documentation) : Membuat perintah-perintah docker

---
+++ command.sh
@@ -0,0 +1,17 @@
+docker version
+
+sudo docker image ls
+
+sudo docker pull nama_image
+
+sudo docker image rm nama_image
+
+sudo docker container ls -a
+
+sudo docker container create --name nama_container image:tag
+
+sudo docker container start nama_container
+
+sudo docker container stop nama_container
+
+sudo docker container rm nama_container
